ec0b49ab25e7d5517f813269ea0454cb5037ab86
yaroslav-pukazov <yaroslav@pukazov.ru>
2024-05-19 16:04:28 +0300

    git init

---
+++ MACROS/PatternsCheckAddin/Install.bat
@@ -0,0 +1,2 @@
+C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe /codebase "%~dp0PatternsCheckAddin.dll"
+@pause
---
+++ MACROS/PatternsCheckAddin/UnInstall.bat
@@ -0,0 +1,2 @@
+C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe /u "%~dp0PatternsCheckAddin.dll"
+@pause
